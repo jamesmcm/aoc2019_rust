c01a134fc85a7df9c3cd6160816cee5388a7d721
James Roger McMurray <jamcmurr@amazon.com>
2020-01-01 14:51:50 +0000

    Fix formatting

--- src/day8.rs
+++ src/day8.rs
@@ -7,7 +7,7 @@ pub fn input_generator(input: &str) -> Vec<char> {
 
 #[aoc(day8, part1)]
 pub fn solve_part1(input: &[char]) -> i32 {
-  solve_part1_inner(input, 25, 6)
+    solve_part1_inner(input, 25, 6)
 }
 
 fn solve_part1_inner(input: &[char], width: usize, height: usize) -> i32 {
@@ -21,11 +21,11 @@ fn solve_part1_inner(input: &[char], width: usize, height: usize) -> i32 {
     let mut ones: i32 = 0;
     let mut twos: i32 = 0;
     for c in chars {
-      match c {
-        '1' => ones += 1,
-        '2' => twos += 1,
-        _ => {}
-      }
+        match c {
+            '1' => ones += 1,
+            '2' => twos += 1,
+            _ => {}
+        }
     }
 
     ones * twos
@@ -33,32 +33,33 @@ fn solve_part1_inner(input: &[char], width: usize, height: usize) -> i32 {
 
 #[aoc(day8, part2)]
 pub fn solve_part2(input: &[char]) -> String {
-  solve_part2_inner(input, 25, 6).iter().fold(String::from(""), |x, y| x + "\n" + y)
+    solve_part2_inner(input, 25, 6)
+        .iter()
+        .fold(String::from(""), |x, y| x + "\n" + y)
 }
 
 fn solve_part2_inner(input: &[char], width: usize, height: usize) -> Vec<String> {
-    let mut chunks = input
-        .chunks(width * height);
+    let mut chunks = input.chunks(width * height);
 
     let mut output: String = chunks.next().unwrap().iter().cloned().collect();
 
     for chunk in chunks {
-      output = output.chars().zip(chunk).map(|x| {
-        match x {
-          ('2', z) => *z,
-          z => z.0,
-          }
-      }
-      ).collect()
+        output = output
+            .chars()
+            .zip(chunk)
+            .map(|x| match x {
+                ('2', z) => *z,
+                z => z.0,
+            })
+            .collect()
     }
 
     let mut out: Vec<String> = Vec::new();
-    for x in output.chars().chunks(width).into_iter() { 
+    for x in output.chars().chunks(width).into_iter() {
         out.push(x.collect::<String>());
-      }
-      out
+    }
+    out
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -67,19 +68,12 @@ mod tests {
 
     #[test]
     fn test_inner1() {
-        assert_eq!(
-            solve_part1_inner(&input_generator(
-                "123456789012"
-            ), 3, 2),
-            1
-        );
+        assert_eq!(solve_part1_inner(&input_generator("123456789012"), 3, 2), 1);
     }
     #[test]
     fn test_inner2() {
         assert_eq!(
-            solve_part2_inner(&input_generator(
-                "0222112222120000"
-            ), 2, 2),
+            solve_part2_inner(&input_generator("0222112222120000"), 2, 2),
             vec!["01", "10"]
         );
     }
